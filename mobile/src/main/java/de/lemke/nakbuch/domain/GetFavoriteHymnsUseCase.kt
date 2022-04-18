cc672139d2c6ae676b9fc3d3f1d64eb25029099e
Leo <leonard.lemke@hs-furtwangen.de>
2022-04-19 00:10:59 +0200

    preparation for non-spaghetti-architecture

--- mobile/src/main/java/de/lemke/nakbuch/domain/GetFavoriteHymnsUseCase.kt
+++ mobile/src/main/java/de/lemke/nakbuch/domain/GetFavoriteHymnsUseCase.kt
@@ -1,9 +1,10 @@
-package de.lemke.nakbuch.domain.model
+package de.lemke.nakbuch.domain
 
 import android.content.Context
 import android.content.SharedPreferences
 import de.lemke.nakbuch.data.hymnDataRepo
 import de.lemke.nakbuch.data.hymnsRepo
+import de.lemke.nakbuch.domain.model.Hymn
 
 class GetFavoriteHymnsUseCase {
     operator fun invoke(
@@ -11,12 +12,12 @@ class GetFavoriteHymnsUseCase {
         sp: SharedPreferences,
         spHymns: SharedPreferences,
         gesangbuchSelected: Boolean,
-    ): List<Hymn> {
+    ): ArrayList<Hymn> {
         return hymnsRepo.getAllHymns(mContext, sp, gesangbuchSelected).filter { hymn ->
             hymnDataRepo.getAllHymnData(
                 gesangbuchSelected,
                 spHymns
             )[hymn.number - 1]?.favorite ?: false
-        }
+        } as ArrayList
     }
 }
